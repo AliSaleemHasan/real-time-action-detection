0d74485e928a77b282a769876376c4750c4ab6a1
ali1saleem1hasan <ali1saleem1hasan@gmail.com>
2022-07-17 10:11:02 +0300

    add evalutaion in train.py file
    evaluate on confusion matrix and plot the results using:
    plot_confusion_matrix and evaluate functions

--- train.py
+++ train.py
@@ -8,16 +8,24 @@ This file is for creating model and train it using tensorFlow
 
 '''
 
+import os 
 import yaml
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 from yaml.loader import SafeLoader
+from keras.utils.np_utils import to_categorical
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import LSTM, Dense
-import numpy as np
-import os 
+from tensorflow.python.keras.metrics import BinaryAccuracy
 from tensorflow.python.keras.callbacks import TensorBoard
-from keras.utils.np_utils import to_categorical
+from sklearn.metrics import confusion_matrix,classification_report
+
+
+
+
+
 
 
 def LSTM_model(modelConfigPath):
@@ -80,6 +88,7 @@ def getDataSet(actions,datasetPath,sequence_length,test_size):
         
         Returns : X_train, X_test, y_train, y_test
     '''
+    start_time = time.perf_counter()
 
     # intialize list to save all sequences for one video in dataset
     sequences = []
@@ -123,6 +132,11 @@ def getDataSet(actions,datasetPath,sequence_length,test_size):
     # split DataSet to train test 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
 
+    end_time = time.perf_counter()
+    print("test")
+    print(f'It took {end_time- start_time :0.2f} second(s) to complete.')
+
+
     return X_train,X_test,y_train,y_test
 
 
@@ -153,7 +167,9 @@ def Train(model,model_path,X_train,y_train,epochs,optimizer,loss,metric,logsPath
     if os.path.exists(model_path) == True:
 
         # load weights into model
-        model = model.load_weights(model_path)
+        model.load_weights(model_path)
+
+        model.compile(optimizer, loss, metrics=[metric])
 
         return model
 
@@ -180,7 +196,124 @@ def Train(model,model_path,X_train,y_train,epochs,optimizer,loss,metric,logsPath
 
 
 
+# helper function to plot confusion matrix 
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues,
+                          size=None):
+    """ (Copied from sklearn website)
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Display normalized confusion matrix ...")
+    else:
+        print('Display confusion matrix without normalization ...')
+
+
+    fig, ax = plt.subplots()
+    if size is None:
+        size = (12, 8)
+    fig.set_size_inches(size[0], size[1])
+
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    ax.set_ylim([-0.5, len(classes)-0.5])
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax, cm
+
 
     
 
 
+
+
+def evaluate_model(model, classes, X_train, X_test, y_train, y_test):
+    '''
+    This function is to  Evaluate accuracy and time cost 
+        Args : 
+            model : model to evaluate on
+            classes : dataset classes
+            tr_X,tr_Y,te_X,te_Y : train's and test's (x and y)
+    
+    '''
+    # start by recording time
+    t0 = time.time()
+
+    # accuracy on train set
+    model.evaluate(X_train, y_train)
+
+    # accuracy on test set
+    model.evaluate(X_test, y_test)
+
+    # get prediction as integers [0,1]
+    y_test_predict  = np.array(model.predict(X_test)).astype(int)
+
+    # git index of predicted values in each test sample
+    y_test_predict = np.argmax(y_test_predict,axis=1)
+
+
+
+    # git index of true values in each test sample
+    y_test = np.array(y_test)
+    y_test = np.argmax(y_test,axis =1)
+
+    # Time cost
+    average_time = (time.time() - t0) / (len(y_train) + len(y_test))
+    print("Time cost for predicting on train and test data is: "
+          "{:.5f} seconds".format(average_time))
+
+    # Plot confucion_matrix (TP,TN,FP,FN)
+    plot_confusion_matrix(
+        y_test, y_test_predict, classes, normalize=False, size=(12, 8))
+    plt.show()
+
+
+
+
+
+
+
+
+# X_train,X_test,y_train,y_test = getDataSet(['posing','not_posing'],'DATA_SET',30,0.2)
+# lstm = LSTM_model('config.yaml')
+
+# model =Train(lstm,"models/weights.h5",X_train,y_train,2000,'Adam','binary_crossentropy',BinaryAccuracy(),'logs')
+
+
+
+# evaluate_model(model,['posing','not_posing'],X_train,X_test,y_train,y_test)
